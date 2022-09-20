877420124c3c334ace956c436c1d2af20d152ac0
Z1J5Y <z1j5y9939@gmail.com>
2022-09-21 01:20:12 +0800

    am-kernels ics2021 initialized without tracing
    am-kernels adc316af6e482e6444a9bd68bafc3a57e2cafdbc cpu-tests,Makefile: support target `gdb`

--- nemu/include/macro.h
+++ nemu/include/macro.h
@@ -26,10 +26,10 @@
 #define MUX_WITH_COMMA(contain_comma, a, b) CHOOSE2nd(contain_comma a, b)
 #define MUX_MACRO_PROPERTY(p, macro, a, b) MUX_WITH_COMMA(concat(p, macro), a, b)
 // define placeholders for some property
-#define __P_DEF_0  X,
-#define __P_DEF_1  X,
-#define __P_ONE_1  X,
-#define __P_ZERO_0 X,
+#define __P_DEF_0  X,        // !!!here is a comma!!!
+#define __P_DEF_1  X,        // CHOOSE2nd(contain_comma a,b) will be expanded
+#define __P_ONE_1  X,        // to CHOOSE2nd(X,a,b) if __P_DEF_* is defined
+#define __P_ZERO_0 X,        // !!!here is a comma!!!
 // define some selection functions based on the properties of BOOLEAN macro
 #define MUXDEF(macro, X, Y)  MUX_MACRO_PROPERTY(__P_DEF_, macro, X, Y)
 #define MUXNDEF(macro, X, Y) MUX_MACRO_PROPERTY(__P_DEF_, macro, Y, X)
