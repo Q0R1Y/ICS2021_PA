9afd4bc014ee71d1d9eda929df12182d9cc596e8
tracer-ics2021 <tracer@njuics.org>
2023-06-27 04:02:44 +0800

    >  compile QAQ

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -37,6 +37,15 @@ static int cmd_q(char *args) {
   return -1;
 }
 
+static int cmd_si(char *args) {
+  int steps;
+  if((steps=atoi(args))>0)
+    cpu_exec(steps);
+  else
+    printf("args error, it should be a integer!\n");
+  return 0;
+}
+
 static int cmd_help(char *args);
 
 static struct {
@@ -47,7 +56,7 @@ static struct {
   { "help", "Display informations about all supported commands", cmd_help },
   { "c", "Continue the execution of the program", cmd_c },
   { "q", "Exit NEMU", cmd_q },
-
+  { "si", "Step into the next n instructions", cmd_si},
   /* TODO: Add more commands */
 
 };
