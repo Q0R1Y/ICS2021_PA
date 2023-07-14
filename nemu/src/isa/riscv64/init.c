6278a932e080679e4a28bb34a0db3967c0f69af0
tracer-ics2021 <tracer@njuics.org>
2023-07-14 18:00:06 +0800

    >  compile QAQ    Static hostname: z1j5y-laptop          Icon name: computer-laptop            Chassis: laptop         Machine ID: bf747253acb940bfb2248a79eb426a42            Boot ID: 5c1b14676d1849cd8c039f73bb1d2785   Operating System: Ubuntu 20.04.6 LTS             Kernel: Linux 5.15.1       Architecture: x86-64  18:00:06 up 3 days,  1:19,  5 users,  load average: 1.04, 1.09, 1.09

--- nemu/src/isa/riscv64/init.c
+++ nemu/src/isa/riscv64/init.c
@@ -22,7 +22,7 @@ static void restart() {
 void init_isa() {
   /* Load built-in image. */
   memcpy(guest_to_host(RESET_VECTOR), img, sizeof(img));
-
+  printf("guest_to_host:\nguest:%u\nhost:%hhn\n",RESET_VECTOR,guest_to_host(RESET_VECTOR));
   /* Initialize this virtual computer system. */
   restart();
 }
--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -8,7 +8,8 @@ static uint8_t *pmem = NULL;
 #else
 static uint8_t pmem[CONFIG_MSIZE] PG_ALIGN = {};
 #endif
-
+// guest is virtual address of memory
+// host is actual index of memory(the array)
 uint8_t* guest_to_host(paddr_t paddr) { return pmem + paddr - CONFIG_MBASE; }
 paddr_t host_to_guest(uint8_t *haddr) { return haddr - pmem + CONFIG_MBASE; }
 
